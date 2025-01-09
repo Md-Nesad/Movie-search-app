701a1ceeeef1051a9963bf37ffb9eb91e4580401
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2025-01-09 14:44:58 +0600

    s

--- src/component/MovieData.jsx
+++ src/component/MovieData.jsx
@@ -10,12 +10,10 @@ const MovieData = () => {
   const [loading, setLoading] = useState(true);
 
   const getMovieData = async () => {
-    setLoading(true);
     const data = await axios.get(
       `http://www.omdbapi.com/?apikey=29c8ed77&s=${searchval}`
     );
     setMovieData(data.data.Search);
-    setLoading(false);
   };
 
   useEffect(() => {
